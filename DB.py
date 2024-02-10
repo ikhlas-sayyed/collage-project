5773c7191d8243f49323f0e4f39db45d706eb240
ikhlas sayyed <ikhlassayyed41@gmail.com>
2024-02-10 15:52:18 +0530

    first

---
+++ DB.py
@@ -0,0 +1,15 @@
+import os
+import datetime
+from mongoengine import connect, Document, StringField, IntField, DateTimeField
+
+# Connect to MongoDB
+connect(os.environ['DATABASE_NAME'], host=os.environ['DATABASE_URI'])
+
+class Student(Document):
+    name = StringField(required=True)
+    roll_no = IntField(required=True)
+    div = StringField(required=True)
+    class_no:IntField(required=True)
+    en_no:IntField(unique=True)
+    created_at = DateTimeField(default=datetime.datetime.now)
+
---
+++ routes/account.py
@@ -0,0 +1,20 @@
+import os
+from flask import Blueprint ,request, jsonify
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from DB import Student
+
+account = Blueprint('account', __name__)
+
+@account.route('/add_student',methods=['POST'])
+def add_student():
+    data=request.json
+    div=['A','B','C','D','E','F']
+    if ("name" in data and "roll_no" in data and "div" in data and "class_no" in data) :
+        if (len(data['name'])>8 and int(data['roll_no'])>0 and data['div'] in div and int(data['class_no'])>0 and int(data['class_no'])<5):
+            student=Student(name=data['name'],roll_no=data['roll_no'],div=data['div'],class_no=data['class_no'])
+            student.save()
+            return jsonify({"success":True,"message": "student add successfully"})
+        else:
+            return jsonify({"success":False,"message":"Incompolete Data"})
+    else:
+        return jsonify({"success":False,"message":"data is Undefinde"})
